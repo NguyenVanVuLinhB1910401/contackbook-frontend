34d58160a405c0220818ec4af45e66125bd979a4
Linh Nguyen <nvvlinh2001@gmail.com>
2022-10-25 19:33:33 +0700

    Hien thi danh sach cac contact lay ve tu server

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <script>
-export default {
-
-}
+import AppHeader from '@/components/AppHeader.vue';
+export default { components: { AppHeader } };
 </script>
-
 <template>
-  <h1>Hello, vuejs</h1>
+  <div id="app">
+    <AppHeader />
+    <div class="container mt-3"><router-view /></div>
+  </div>
 </template>
-
 <style>
 .page {
   max-width: 400px;
